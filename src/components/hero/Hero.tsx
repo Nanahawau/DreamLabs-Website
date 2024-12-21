68fc74d74b8a38b1845c2ba44ff0324fe0129db6
Ali Adeku <aliadeku.aam@gmail.com>
2024-12-21 12:37:50 +0100

    create navbar and hero section

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './hero.module.scss';
+
+const Hero = () => {
+  return (
+    <div className={styles.container}>
+      We’re an independent group of designers, builders, writers, and entrepreneurs obsessed with
+      &quot;launching and growing brands.&quot; We work hard to &quot;toe a fine line between
+      technology and art,&quot; pushing businesses into their best selves. Our work covers branding,
+      content strategy, and software development.
+    </div>
+  );
+};
+
+export default Hero;
