b3ffeee68237d199e7852b70703307de603d2693
Nana Adeku <nana@paystack.com>
2024-12-30 23:23:28 +0000

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Home from './pages/home/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import Header from './components/header/Header';
 import Hertitude from './pages/hertitude/Hertitude';
 
 const App = () => {
