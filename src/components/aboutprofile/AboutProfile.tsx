febefa14cbcf7bb443e8ca243a1c9428f612c351
Nana Adeku <nanaadeku@Nanas-MacBook-Pro.local>
2025-06-15 16:29:48 +0100

    Remove nav elements

--- src/components/aboutprofile/AboutProfile.tsx
+++ src/components/aboutprofile/AboutProfile.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './AboutProfile.module.scss';
 
-const profile = {
-  'Busayo Oladejo': '/images/busayo.jpg',
-  'Celeste Ojatula': '/images/busayo.jpg',
-  'Ifoghale Eguwe': '/images/busayo.jpg',
-  'Nana Adeku': '/images/busayo.jpg'
-};
+// const profile = {
+//   'Busayo Oladejo': '/images/busayo.jpg',
+//   'Celeste Ojatula': '/images/busayo.jpg',
+//   'Ifoghale Eguwe': '/images/busayo.jpg',
+//   'Nana Adeku': '/images/busayo.jpg'
+// };
 const AboutProfile = () => {
   return (
     <div className={styles.container}>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './header.module.scss';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -12,26 +12,26 @@ const Header = () => {
       </div>
       <nav>
         <ul className={styles.navlist}>
-          <li className={styles.navitem}>
-            <NavLink to="/services" className={styles.navitem}>
-              SERVICES
-            </NavLink>
-          </li>
-          <li className={styles.navitem}>
-            <NavLink to="/work" className={styles.navitem}>
-              WORK
-            </NavLink>
-          </li>
-          <li className={styles.navitem}>
-            <NavLink to="/about" className={styles.navitem}>
-              ABOUT
-            </NavLink>
-          </li>
-          <li className={styles.navitem}>
-            <NavLink to="/contact" className={styles.navitem}>
-              CONTACT
-            </NavLink>
-          </li>
+          {/*<li className={styles.navitem}>*/}
+          {/*  <NavLink to="/services" className={styles.navitem}>*/}
+          {/*    SERVICES*/}
+          {/*  </NavLink>*/}
+          {/*</li>*/}
+          {/*<li className={styles.navitem}>*/}
+          {/*  <NavLink to="/work" className={styles.navitem}>*/}
+          {/*    WORK*/}
+          {/*  </NavLink>*/}
+          {/*</li>*/}
+          {/*<li className={styles.navitem}>*/}
+          {/*  <NavLink to="/about" className={styles.navitem}>*/}
+          {/*    ABOUT*/}
+          {/*  </NavLink>*/}
+          {/*</li>*/}
+          {/*<li className={styles.navitem}>*/}
+          {/*  <NavLink to="/contact" className={styles.navitem}>*/}
+          {/*    CONTACT*/}
+          {/*  </NavLink>*/}
+          {/*</li>*/}
         </ul>
       </nav>
     </div>
